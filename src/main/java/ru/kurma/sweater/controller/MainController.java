cb790074fa321095a5e6b1b83d770aa9aac768e3
SanKur <kurmasasha@gmail.com>
2019-08-18 01:34:07 +0300

    Таймлиф заменили на фримаркер

--- src/main/java/ru/kurma/sweater/controller/MainController.java
+++ src/main/java/ru/kurma/sweater/controller/MainController.java
@@ -2,6 +2,7 @@ package ru.kurma.sweater.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,8 +26,15 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        model.put("messages", messageRepository.findAll());
+    public String main(@RequestParam(required = false, defaultValue = "") String filter,  Model model) {
+
+        if (filter != null && !filter.isEmpty()) {
+            model.addAttribute("messages", messageRepository.findByTag(filter));
+        } else {
+            model.addAttribute("messages", messageRepository.findAll());
+        }
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
@@ -42,16 +50,6 @@ public class MainController {
 
         model.put("messages", messageRepository.findAll());
 
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        if (filter != null && !filter.isEmpty()) {
-            model.put("messages", messageRepository.findByTag(filter));
-        } else {
-            model.put("messages", messageRepository.findAll());
-        }
         return "main";
     }
 }
