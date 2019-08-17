08c5b36ba35585ca749edb21c6043e82440e3c71
SanKur <kurmasasha@gmail.com>
2019-08-18 00:42:58 +0300

    Добавлен автор к сообщениям

--- src/main/java/ru/kurma/sweater/controller/MainController.java
+++ src/main/java/ru/kurma/sweater/controller/MainController.java
@@ -1,10 +1,12 @@
 package ru.kurma.sweater.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.kurma.sweater.model.Message;
+import ru.kurma.sweater.model.User;
 import ru.kurma.sweater.repository.MessageRepository;
 
 import java.util.Map;
@@ -29,10 +31,17 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+        public String add(
+                @AuthenticationPrincipal User user,
+                @RequestParam String text,
+                @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
+
         messageRepository.save(message);
+
         model.put("messages", messageRepository.findAll());
+
         return "main";
     }
 
